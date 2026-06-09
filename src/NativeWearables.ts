94116f614132816e99a5e989bf25dfd5ea6b754c
harishkumar69 <harishkumar6317@gmail.com>
2026-06-09 11:33:33 +0530

    fix: update getApplicationContext return type to WearableMessage

--- src/NativeWearables.ts
+++ src/NativeWearables.ts
@@ -1,9 +1,10 @@
 import { TurboModuleRegistry, type TurboModule } from 'react-native';
+import type { WearableMessage } from '.';
 
 export interface Spec extends TurboModule {
   sendMessage(message: Object): Promise<void>;
   updateApplicationContext(context: Object): Promise<void>;
-  getApplicationContext(): Promise<Object | null>;
+  getApplicationContext(): Promise<WearableMessage | null>;
   isPaired(): Promise<boolean>;
   isReachable(): Promise<boolean>;
   isWatchAppInstalled(): Promise<boolean>;
